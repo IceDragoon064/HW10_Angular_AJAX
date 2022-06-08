580388943c2b30dc4de375527ce3d10f65c5c831
Leon Lu <ltlu1993@gmail.com>
2022-06-08 15:35:31 -0400

    Focus and blur example

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -3,19 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css']
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'app';
 
-  myKeyDown(event) {
-    console.log(event);
-    console.log(event.key);
-  }
-
-  myKeyUp(event){
-    console.log(event);
-    console.log(event.key);
+  myFocusEvent(description) {
+    console.log(description)
   }
 }
