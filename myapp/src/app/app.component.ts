cd8211f48f8edaea48d67162fb3eed5e8b0198f4
Leon Lu <ltlu1993@gmail.com>
2022-06-08 15:28:38 -0400

    Key down example

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -3,24 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css']
 })
 
 export class AppComponent {
   title = 'app';
-  count: number;
 
-  constructor(){
-    this.count = 0;
-  }
-
-  myMouseEnter() {
-    console.log("Mouse entered!")
-  }
-
-  mouseleave(){
-    this.count = this.count + 1;
-
-    console.log("Mouse has left. Count: " + this.count);
+  myKeyDown(event) {
+    console.log(event)
+    console.log(event.key)
   }
 }
