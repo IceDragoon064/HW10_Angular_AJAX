eda9d3af9efe778aeb2611a2ee04dccc4bb1975b
Leon Lu <ltlu1993@gmail.com>
2022-06-08 14:40:13 -0400

    Callback Example

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,37 +1,32 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
+// This component consumes the re-usable service.
 @Component({
-selector: 'app-root',
-template: `<button (click)="getSomeData()">Test GET Request</button>
-<ul>
-<li *ngFor="let myData of namesArray">{{myData.first}}
-{{myData.last}}</li>
-</ul>`
+  selector: 'app-root',
+  template: `
+    <h3>Please enter the customer information:</h3>
+    <child [callParent]="parentFuncRef" [city]="op"></child>
+    {{dataFromChild}}
+  `
 })
 
 
+
 export class AppComponent {
-  namesArray!: Array<any>;
-  _http:HttpClient;
+  parentFuncRef: Function;
+  operations: Array<any>;
+  dataFromChild: string;
+  op: string;
+  // This function is called by the Angular framework after
+  // the constructor executes.
+  public ngOnInit() {
+    this.parentFuncRef = this.myCallBackFunction.bind(this);
+  }
 
-  // Since we are using a provider above we can receive
-  // an instance through an instructor.
-  constructor(private http: HttpClient) {
-    this._http = http;
-}
-
-getSomeData() {
-  this._http.get<any>('./assets/test.json')
-    // Get data and wait for result.
-    .subscribe(result => {
-      this.namesArray = result;
-    },
-
-    error =>{
-      // Let user know about the error.
-      alert(error);
-      console.error(error)
-    })
+  // This function can be called by child.
+  public myCallBackFunction(streetAddress: string, city: string) {
+    this.dataFromChild =
+     "Street Address: " + streetAddress + " " +
+      "City: " + city;
   }
 }
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './app.child';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
