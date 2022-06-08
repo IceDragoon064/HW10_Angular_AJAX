9e75e5843d6ff52d377ffe2b81a1d4f83e27ec37
Leon Lu <ltlu1993@gmail.com>
2022-06-08 14:21:25 -0400

    Example 1 codes

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+template: `<button (click)="getSomeData()">Test GET Request</button>
+<ul>
+<li *ngFor="let myData of namesArray">{{myData.first}}
+{{myData.last}}</li>
+</ul>`
 })
+
+
 export class AppComponent {
-  title = 'myapp';
+  namesArray!: Array<any>;
+  _http:HttpClient;
+
+  // Since we are using a provider above we can receive
+  // an instance through an instructor.
+  constructor(private http: HttpClient) {
+    this._http = http;
+}
+
+getSomeData() {
+  this._http.get<any>('./assets/test.json')
+    // Get data and wait for result.
+    .subscribe(result => {
+      this.namesArray = result;
+    },
+
+    error =>{
+      // Let user know about the error.
+      alert(error);
+      console.error(error)
+    })
+  }
 }
